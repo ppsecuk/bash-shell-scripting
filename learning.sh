68870f183109a28c55c77a18aaf97cc7fbf88778
pavel_psecuk <pavel.psecuk@bwt.ee>
2020-04-19 19:48:13 +0300

    first commit

---
+++ learning.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Tell the user the script is starting
+echo "Starting the script..."
+
+# Setting variables
+PURPOSE="learning purposes"
+
+# Printing text with variable
+echo "Hello, this file is created for ${PURPOSE}."
+
+# Tell the user the script is closing
+echo "Closing the script..."
